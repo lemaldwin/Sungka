c21ef776fefa5748135a2edddc9442481e03200c
lemuelaldwin@yahoo.com.ph <lemuelaldwin@yahoo.com.ph>
2020-10-21 04:34:25 +0800

    Updated menu.bash

--- menu.bash
+++ menu.bash
@@ -6,4 +6,19 @@ echo "2. Two-Player"
 echo "3. Help"
 echo "4. Exit"
 
+read -p "Choice: " selection
 
+case $selection in
+	1) echo "Starting Game in Single Player Mode"
+		;;
+	2) echo "Starting Game in Two-Player Mode"
+		;;
+	3) echo "Help"
+		;;
+	4) echo "Bye"
+		exit 0
+		;;
+	*) echo "Invalid choice. Exiting..."
+		;;
+esac
+exit 0
